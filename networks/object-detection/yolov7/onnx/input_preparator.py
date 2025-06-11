f9f8720ed8624bef7f35abb2cf3eb25944f17702
Quentin Muller <qmuller@kalrayinc.com>
2025-06-11 16:44:27 +0200

    [NETWORKS] remove redundant networks
    JIRA: https://kalray.atlassian.net/browse/CVAPPS-405
    Link: https://gerrit.kalray.eu/id/If0f0f172a9825a31d125ad25b11b471dc32eb8db
    
    [NETWORKS] remove redundant networks: YOLOv8 n, s, m, l
    
    JIRA: https://kalray.atlassian.net/browse/CVAPPS-405
    Link: https://gerrit.kalray.eu/id/I1b850f5713c1557cb1c9d54bacc6fb61dad2f2d8
    
    [NETWORKS] add YOLOv7 and YOLOv7-tiny full model, remove olds
    
    JIRA: https://kalray.atlassian.net/browse/CVAPPS-388
    JIRA: https://kalray.atlassian.net/browse/CVAPPS-370
    JIRA: https://kalray.atlassian.net/browse/CVAPPS-304
    JRIA: https://kalray.atlassian.net/browse/CVAPPS-405
    Link: https://gerrit.kalray.eu/id/Ic3fa6a70b1bb9e31d553e1075a74ed143c5e5e2b
    
    [NETWORKS] remove YOLOv5s, YOLOv5s6, YOLOv5s6
    
    JIRA: https://kalray.atlassian.net/browse/CVAPPS-387
    Link: https://gerrit.kalray.eu/id/I68a41a3a379e20746ac4388a67b439a436edcd55

--- networks/object-detection/yolov7/onnx/input_preparator.py
+++ networks/object-detection/yolov7/onnx/input_preparator.py
@@ -1,80 +1,52 @@
 #!/usr/bin/env python3
+
+###
+# Copyright (C) 2025 Kalray SA. All rights reserved.
+# This code is Kalray proprietary and confidential.
+# Any use of the code for whatever purpose is subject
+# to specific written permission of Kalray SA.
+###
+
 import sys
-import numpy
 import cv2
-import os
+import numpy
 import itertools as it
-import math
-from collections import OrderedDict
+
+from PIL import Image
 
 
-IMG_RES = 2 ** 8
-IMG_SIZE = (640, 640)
-PADDED_COLOR = (114, 114, 114)
+def letterbox_image(image, size):
+    """ resize image with unchanged aspect ratio using padding """
+    image = Image.fromarray(image)
+    iw, ih = image.size
+    w, h = size
+    scale = min(w/iw, h/ih)
+    nw = int(iw*scale)
+    nh = int(ih*scale)
+    image = image.resize((nw,nh), Image.BICUBIC)
+    new_image = Image.new('RGB', size, (114,114,114))
+    new_image.paste(image, ((w-nw)//2, (h-nh)//2))
+    assert new_image.size == size
+    return new_image
 
 
-def letterbox(
-        img,
-        new_shape=IMG_SIZE,
-        color=PADDED_COLOR,
-        auto=False,
-        scaleFill=False,
-        scaleup=True,
-        auto_size=32
-):
-
-    # Resize image to a 32-pixel-multiple rectangle https://github.com/ultralytics/yolov3/issues/232
-    shape = img.shape[:2]  # current shape [height, width]
-    if isinstance(new_shape, int):
-        new_shape = (new_shape, new_shape)
-
-    # Scale ratio (new / old)
-    r = min(new_shape[0] / shape[0], new_shape[1] / shape[1])
-    if not scaleup:  # only scale down, do not scale up (for better test mAP)
-        r = min(r, 1.0)
-
-    # Compute padding
-    ratio = r, r  # width, height ratios
-    new_unpad = int(round(shape[1] * r)), int(round(shape[0] * r))
-    dw, dh = new_shape[1] - new_unpad[0], new_shape[0] - new_unpad[1]  # wh padding
-    if auto:  # minimum rectangle
-        dw, dh = numpy.mod(dw, auto_size), numpy.mod(dh, auto_size)  # wh padding
-    elif scaleFill:  # stretch
-        dw, dh = 0.0, 0.0
-        new_unpad = (new_shape[1], new_shape[0])
-        ratio = new_shape[1] / shape[1], new_shape[0] / shape[0]  # width, height ratios
-
-    dw /= 2  # divide padding into 2 sides
-    dh /= 2
-
-    if shape[::-1] != new_unpad:  # resize
-        img = cv2.resize(img, new_unpad, interpolation=cv2.INTER_LINEAR)
-    top, bottom = int(round(dh - 0.1)), int(round(dh + 0.1))
-    left, right = int(round(dw - 0.1)), int(round(dw + 0.1))
-    img = cv2.copyMakeBorder(img, top, bottom, left, right, cv2.BORDER_CONSTANT, value=color)  # add border
-    return img, ratio, (dw, dh)
-
-
-def prepare_img(img0, letter_box=True, out_dtype=numpy.float32):
-    new_h, new_w = IMG_SIZE
-    if letter_box:
-        img = letterbox(img0, new_shape=(new_w, new_h), auto_size=64)[0]
-    else:
-        img = numpy.asarray(img0, dtype=numpy.uint8, order='C')
-        img = numpy.flip(img, axis=-1)
-        # resize dimension order is (height,width) in numpy but (width, height) in opencv
-        if img0.shape[0:2] != (new_h, new_w):
-            img = cv2.resize(img0, (new_w, new_h), interpolation=cv2.INTER_LINEAR)
-    # Get the Values between 0 and 1 - BGR to RGB
-    img = img[:, :, ::-1].astype(out_dtype) / 255
-    return img
+def prepare_img(mat, out_dtype=numpy.float32):
+    mat = numpy.asarray(mat, dtype=numpy.uint8, order='C')
+    mat = numpy.flip(mat, axis=-1)
+    model_image_size = (640, 640)
+    boxed_image = letterbox_image(mat, tuple(reversed(model_image_size)))
+    image_data = numpy.array(boxed_image, dtype='float32')
+    image_data /= 255.
+    return image_data.astype(out_dtype)
 
 
 def image_stream(filename):
-    ''' Read and prepare the sequence of images of <filename>.
-      If <filename> is an int, use it as a webcam ID.
-      Otherwise <filename> should be the name of an image, video
-      file, or image sequence of the form name%02d.jpg '''
+    """ 
+    Read and prepare the sequence of images of <filename>.
+    If <filename> is an int, use it as a webcam ID.
+    Otherwise <filename> should be the name of an image, video
+    file, or image sequence of the form name%02d.jpg
+    """
     try: src = int(filename)
     except ValueError: src = filename
     stream = cv2.VideoCapture(src)
@@ -88,7 +60,7 @@ def image_stream(filename):
 
 
 def batches_extraction(stream):
-    ''' extract batches of images from a python generator of prepared images '''
+    """ extract batches of images from a python generator of prepared images """
     batch = 1
     while True:
         imgs = list(it.islice(stream, batch))
@@ -111,5 +83,5 @@ if __name__ == '__main__':
         exit(1)
     stream = it.chain(*map(image_stream, sys.argv[2:]))
     with open(sys.argv[1], 'w') as dest:
-        for imgs in batches_extraction(stream):
-            imgs.tofile(dest, '')
+      for imgs in batches_extraction(stream):
+        imgs.tofile(dest, '')
